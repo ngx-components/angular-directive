b718c4c4888d8f8ac51148613376c9da9b1a3d43
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-29 11:55:53 +0200

    fix(index): fix imports

--- ngx/_build/index.js
+++ ngx/_build/index.js
@@ -1,8 +1,8 @@
 import angular from 'angular';
 import config from './config';
 
-import registerDirective from './ngx-directive.directive.js';
-import registerController from './ngx-directive.controller.js';
+import registerDirective from './ngx-directive.directive';
+import registerController from './ngx-directive.controller';
 
 export default function(ngModule, options){
   angular.merge(config, options);
